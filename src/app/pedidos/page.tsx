0065105d60ad7d184c0cf9e1893e612fe58489a4
Brimanda <br.miranda@duocuc.cl>
2024-10-15 05:18:26 -0300

    Update page.tsx

--- src/app/pedidos/page.tsx
+++ src/app/pedidos/page.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "@/components/component/auth/AuthProvider";
 import { HeaderComponent } from "@/components/component/home/header";
 import { FooterComponent } from "@/components/component/home/footer";
-import { ReservasAnteriores } from "@/components/component/home/reservas-anterioresssss";
+import { ReservasAnteriores } from "@/components/component/home/reservas-anteriores";
 
 export default function Nosotros(){
     return (
